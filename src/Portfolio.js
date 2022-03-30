3febd82c1bf3cf96f9792c370c1b3bac9dd7c04a
MuokaPWambua <muokapwambua@gmail.com>
2022-03-31 01:01:23 +0300

    commit

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,13 +5,18 @@ export default function Portfolio (){
     return(
     <>
      <div className='p-bg'>
-          <h1 align='center'> Portfolio</h1>
+          <div style={{textAlign:'center'}} className='mt-2'>  
+          <h1 align='center'>Portfolio</h1>
+          <p align='center'>My sample projects built in reactjs and python for api .</p> 
+          </div>
           <div id={'p-animate'} ></div>
      </div>
      <div className={'r-me'}>
          <h2 align={'center'}> How To Reach Me </h2> 
-         <p align='center'>linkedin tiwter gmail whatsapp</p> 
-     </div>
+           <p align='center'>Ask me anything?</p>  
+           <p align='center'>linkedin tiwter gmail whatsapp</p> 
+
+    </div>
     </>    
 )
 
