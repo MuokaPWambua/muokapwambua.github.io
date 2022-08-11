ef5f98c5ad6514db206e8f3ebd2371efdd5b5fe9
MuokaPWambua <muokapwambua@gmail.com>
2022-08-11 09:33:03 +0300

    update commit

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './Portfolio.css'
+import twitter from './twitter.png'
+import gmail from './gmail.png'
+import whatsapp from './whatsapp.png'
+import li from './li.png'
 
 export default function Portfolio (){
     return(
     <>
      <div className='p-bg'>
-          <div style={{textAlign:'center'}} className='mt-2'>  
-          <h1 align='center'>Portfolio</h1>
-          <p align='center'>My sample projects built in reactjs and python for api .</p> 
+          <div className='my-2'>  
+            <h1>Portfolio</h1>
+            <p>My sample projects built in reactjs(Frontend), python(Backend) and postgresql(database) .</p> 
           </div>
-          <div id={'p-animate'} ></div>
+          <div id={'p-animate'}></div>
      </div>
      <div className={'r-me'}>
          <h2 align={'center'}> How To Reach Me </h2> 
-           <p align='center'>Ask me anything?</p>  
-           <p align='center'>linkedin tiwter gmail whatsapp</p> 
-
+         <p align='center'>Ask me anything?</p>  
+         <div align='center'>
+            <img width={60} height={60} src={li} alt='linkedin' className='mx-1'/>
+            <img width={60} height={60} src={twitter} alt='twitter' className='mx-1'/>
+            <img width={60} height={60} src={whatsapp} alt='whatsapp' className='mx-1'/>
+            <img width={60} height={60} src={gmail} alt='gmail' className='mx-1'/>
+         </div>
+        <p align='center' className='lead my-3'><u>Back</u></p>
     </div>
     </>    
 )
