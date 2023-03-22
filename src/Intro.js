87e28ab06e51ae44b022bff251edc6714713d9f4
MuokaPWambua <muokapwambua@gmail.com>
2023-03-22 09:23:22 +0300

    update

--- src/Intro.js
+++ src/Intro.js
@@ -5,13 +5,13 @@ const Intro = ({setPortfolio})=> {
     return(
     <div className="App">
     <div className='container'>
-        <h1 align="center">Hi 👋, I'm Patrick Muoka Wambua</h1>
-        <h3 className='lead ' align="center" >
-        
-        A passionate JavaScript|Python developer and an aspiring Data Scientist. With
-        5+ years developing and maintaining scalable, efficient, and reliable applications 👨‍💻 😎.
-        
-        <a href='https://github.com/MuokaPWambua' target='_blank' rel="noreferrer"> Github</a></h3>
+        <h1 align="center">Hi 👋, I'm Patrick Muoka</h1>
+        <h3 className='lead fw-normal' align="center" >            
+            A passionate JavaScript | Python developer and an aspiring Data Scientist. With
+            5+ years developing and maintaining scalable, efficient, and reliable applications 👨‍💻 😎.
+            
+            <a href='https://github.com/MuokaPWambua' target='_blank' rel="noreferrer"> Github</a>
+        </h3>
         <br/>
         <center>
         <button style={{align:'center',padding:'1', width:'35%', backgroundColor:'aqua'}} 
